021060f352d4b6c4dbbd8cab765c2908903c57eb
Mikhail Lukin <mishalykin@mail.ru>
2025-04-18 14:51:41 +0700

    feat: decompose

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -19,8 +19,6 @@ import { IconButton } from '@alfalab/core-components/icon-button/modern';
 
 import { useClickOutside } from '@alfalab/hooks';
 import { MagnifierMIcon } from '@alfalab/icons/glyph/dist/MagnifierMIcon';
-import { CopyLineMIcon } from '@alfalab/icons/glyph/dist/CopyLineMIcon';
-import { ArrowRightCurvedMIcon } from '@alfalab/icons-glyph/ArrowRightCurvedMIcon';
 import { PlayCircleMIcon } from '@alfalab/icons-glyph/PlayCircleMIcon';
 
 import {
@@ -28,7 +26,6 @@ import {
     Asset,
     ClickedElement,
     CopyType,
-    DeprecatedAssets,
     DeprecatedType,
     IconPackageName,
     IconsInfo,
@@ -52,95 +49,13 @@ import animationJson from 'ui-primitives/animations/test.json';
 import './Demo.css';
 import LottieIcon from './LottieIcon';
 import { getDeprecatedAssets } from '../helpers/get-deprecated-assets';
+import { ASSET_TO_PACKAGE_NAME } from '../constants/asset-to-package-name';
+import { getOptionsList } from './OptionList';
 
 const ASSET_OPTIONS = getKeys(Asset).map((key) => ({
     key: Asset[key],
     content: getPackageNameAsset(Asset[key], true),
 }));
-
-const getOptionContentCopy = (text: string) => (
-    <div className='option-content'>
-        <Typography.Text view='component-primary' color='primary'>
-            {text}
-        </Typography.Text>
-
-        <CopyLineMIcon />
-    </div>
-);
-
-const getOptionContentDeprecated = (text: string, replace: boolean) => (
-    <div
-        className={cn('option-content', {
-            'option-no-replace': !replace,
-        })}
-    >
-        <Typography.Text
-            view={replace ? 'component-primary' : 'primary-small'}
-            color={replace ? 'primary' : 'tertiary'}
-        >
-            {text}
-        </Typography.Text>
-
-        {replace && <ArrowRightCurvedMIcon color='var(--color-light-graphic-tertiary)' />}
-    </div>
-);
-
-const COPY_OPTIONS = [
-    { key: CopyType.NAME, content: getOptionContentCopy('Имя иконки') },
-    { key: CopyType.REACT_NAME, content: getOptionContentCopy('Имя компонента') },
-    { key: CopyType.IMPORT_CODE, content: getOptionContentCopy('Код для импорта') },
-];
-
-const DEPRECATED_OPTION_WITH_REPLACE = [
-    {
-        key: DeprecatedType.DEPRECATED,
-        content: getOptionContentDeprecated('Перейти к замене', true),
-    },
-];
-
-const DEPRECATED_OPTION_WITHOUT_REPLACE = [
-    {
-        key: DeprecatedType.NO_REPLACE,
-        content: getOptionContentDeprecated(
-            'Замены нет. Попробуйте найти другую иконку, подходящую под вашу задачу.',
-            false,
-        ),
-    },
-];
-
-const ASSET_TO_PACKAGE_NAME = {
-    icons: [
-        { value: 'glyph', label: 'Glyph' },
-        { value: 'rocky', label: 'Rocky' },
-        { value: 'ios', label: 'iOS' },
-        { value: 'android', label: 'Android' },
-        { value: 'corp', label: 'Corp' },
-        { value: 'invest', label: 'Invest' },
-        { value: 'site', label: 'Site' },
-        { value: 'classic', label: 'Classic (deprecated)' },
-    ],
-    logotype: [
-        { value: 'logo', label: 'Logo' },
-        { value: 'logo-am', label: 'Logo-am' },
-        { value: 'logotype', label: 'Logotype' },
-    ],
-    flag: [],
-    animation: [],
-};
-
-function getOptionsList(iconName: string, packageName: string, deprecatedIcons: DeprecatedAssets) {
-    if (packageName === IconPackageName.CLASSIC) {
-        return DEPRECATED_OPTION_WITHOUT_REPLACE;
-    }
-
-    if (!deprecatedIcons.hasOwnProperty(iconName)) {
-        return COPY_OPTIONS;
-    } else if (deprecatedIcons[iconName].replacement) {
-        return DEPRECATED_OPTION_WITH_REPLACE;
-    } else {
-        return DEPRECATED_OPTION_WITHOUT_REPLACE;
-    }
-}
 
 const IconsGlyph: AnyIcon = {};
 const IconsRocky: AnyIcon = {};
